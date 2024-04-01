0f8d007315e1f38d23de163a4225f5dc579d0e1f
Kristian Quirapas <kmquirapas@gmail.com>
2024-04-01 16:05:49 +0800

    add: new command base

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # hammer-rs
-Sick and tired of Hashlip's ugly ass ergonomics.
+Sick and tired of Hashlip's ugly ass ergonomics for Solana? hammer is the tool just for you. Generate your Token assets with better UX.
 
+Currently supporting:
+1. Fungible Tokens (SPL Tokens, Token 2022)
 
+Will be supporting:
+1. Semi-Fungible Tokens
+2. Non-Fungible Tokens
+3. Programmable Non-Fungible Tokens
 
+# Commands
+
+`new` - Instantiate a new project.
+
+`config` - Change the configuration of the metadata standard
+
+`smash` - Flatten the shit out of your layers from '/layers/ to '/assets'.
+
+`layers` - Reorganize the ordering of layers with TUI.
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,106 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use colored::Colorize;
+use serde::Serialize;
+use std::{error::Error, fs, path::Path};
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    // Path to create new workspace in
     #[arg(short, long)]
-    name: String,
+    path: Option<String>,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    // Solana token standard to generate
+    // Reference: https://developers.metaplex.com/core/create-asset
+    #[arg(short, long)]
+    standard: Option<TokenStandard>,
+
+    #[command(subcommand)]
+    command: Commands,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Subcommand, Debug)]
+enum Commands {
+    // Initialize a workspace
+    New { name: String },
+}
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
+enum TokenStandard {
+    #[default]
+    Fungible,
+}
+
+fn print_header() {
+    println!("{}", "  _                                         ".green());
+    println!("{}", " | |                                        ".green());
+    println!("{}", " | |                                        ".green());
+    println!("{}", " | |__   __ _ _ __ ___  _ __ ___   ___ _ __ ".green());
+    println!(
+        "{}",
+        " | '_ \\ / _` | '_ ` _ \\| '_ ` _ \\ / _ \\ '__| ".green()
+    );
+    println!("{}", " | | | | (_| | | | | | | | | | | |  __/ |   ".green());
+    println!(
+        "{}",
+        " |_| |_|\\__,_|_| |_| |_|_| |_| |_|\\___|_|   ".green()
+    );
+    println!();
+    println!(
+        "    {} {} {} ",
+        "Flatten the".green(),
+        "shit".bold().green(),
+        "out of your assets".green()
+    );
+    println!();
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // display ASCII art
+    print_header();
+
+    let args = Cli::parse();
+
+    match &args.command {
+        Commands::New { name } => {
+            println!("FUNGIBLE STANDARD");
+            println!("PATH: {:?}", args.path);
+            println!("NAME: {:?}", name);
+
+            let path_string = match args.path {
+                Some(p) => p,
+                None => ".".to_string(),
+            };
+
+            fs::create_dir_all(Path::new(&format!(
+                "{}/{}/assets",
+                path_string,
+                name.to_lowercase()
+            )))?;
+
+            //
+            // Some(path_string) => println!("{}", path_string),
+            // // Some(path_string) => fs::create_dir_all(Path::new(path_string))?,
+            // None => fs::create_dir_all(Path::new("./fungible"))?,
+        }
     }
+
+    Ok(())
 }
+
+// Some(path_string) => {
+//     println!("{} {} at {}", "Created".bold().green(), name, path_string)
+// }
+// None => {
+//     print_header();
+//     println!(
+//         "{} no path supplied. Using current directory",
+//         "Warning".yellow()
+//     );
+//     println!(
+//         "{} `{}` in the current directory",
+//         "Created".bold().green(),
+//         name
+//     )
+// }
